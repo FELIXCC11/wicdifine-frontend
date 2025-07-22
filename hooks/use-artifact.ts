af3f63cdd3ba5190109309e646df4885e61032ce
FELIXCC11 <thuanchau0866@gmail.com>
2025-07-21 20:43:55 -0700

    fix: Update import path in hooks/use-artifact.ts to use relative path

--- hooks/use-artifact.ts
+++ hooks/use-artifact.ts
@@ -1,7 +1,7 @@
 'use client';
 
 import useSWR from 'swr';
-import { UIArtifact } from '@/components/artifact';
+import { UIArtifact } from '../src/components/artifact';
 import { useCallback, useMemo } from 'react';
 
 export const initialArtifactData: UIArtifact = {
